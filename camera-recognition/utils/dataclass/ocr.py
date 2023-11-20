931c89344396466f5985cba5607c6a7839a06824
Odoo-Tangerine <128609047+Odoo-Tangerine@users.noreply.github.com>
2023-11-20 21:56:20 +0700

    Update ocr.py

--- camera-recognition/utils/dataclass/ocr.py
+++ camera-recognition/utils/dataclass/ocr.py
@@ -1,5 +1,4 @@
 import re
-from numpy import nparray
 from typing import List
 from dataclasses import dataclass
 from easyocr import Reader
@@ -10,23 +9,23 @@ class OCR:
     reader: Reader = Reader(['en'])
 
     @staticmethod
-    def clean_character(character):
-        character_cleaned = re.sub(r'[^\w\s]', '', character)
+    def clean_character(text):
+        character_cleaned = re.sub(r'[^\w\s]', '', text)
         character_cleaned = re.sub(r'\s', '', character_cleaned)
         return character_cleaned
 
     @staticmethod
-    def _validate_license_plate_character(character_lst: List[str]) -> bool:
-        if not character_lst or \
-            len(character_lst) != 2 or \
-            not character_lst[0][:2].isnumeric() or \
-            not character_lst[0][-1].isalpha() or \
-            len(character_lst[1]) not in [4, 5] or \
-            not character_lst[1].isnumeric():
+    def _validate_license_plate_character(result):
+        if not result or \
+                len(result) != 2 or \
+                not result[0][:2].isnumeric() or \
+                not result[0][-1].isalpha() or \
+                len(result[1]) not in [4, 5] or \
+                not result[1].isnumeric():
             return False
         return True
 
-    def extract_character(self, threshold_image: nparray):
+    def extract_license_plate(self, threshold_image):
         results = self.reader.readtext(threshold_image)
         character_lst = [self.clean_character(character) for _, character, score in results]
         is_valid = self._validate_license_plate_character(character_lst)
