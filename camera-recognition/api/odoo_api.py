057e3b7d14ed3c19c18add0d4e618da8edf21dff
Odoo-Tangerine <128609047+Odoo-Tangerine@users.noreply.github.com>
2023-11-20 21:55:35 +0700

    Update odoo_api.py

--- camera-recognition/api/odoo_api.py
+++ camera-recognition/api/odoo_api.py
@@ -1,8 +1,13 @@
 import aiohttp
 import logging
+from dotenv import load_dotenv
+from os import environ, path
 from dataclasses import dataclass
 from typing import Dict, Any, Optional
-from ..utils.variables.constants import Const
+
+
+BASEDIR = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(BASEDIR, '.env'))
 
 
 _logger = logging.getLogger(__name__)
@@ -10,7 +15,7 @@ _logger = logging.getLogger(__name__)
 
 @dataclass
 class OdooAPI:
-    odoo_url: str = Const.ODOO_SERVER_DOMAIN.value
+    odoo_url: str = environ.get('ODOO_SERVER_DOMAIN')
 
     @staticmethod
     async def make_request(url: str, method: str, data: Optional[Dict[str, Any]] = None, **kwargs):
@@ -27,14 +32,23 @@ class OdooAPI:
 
     @staticmethod
     def check_response(response):
-        if response and response.get('status') == 200:
-            return response.get('data')
+        if response:
+            if response.get('status') and response.get('status') == 200:
+                return response.get('data', 'Success')
         return None
 
     async def authenticate_into_parking(self, license_plate: str) -> Optional[Dict[str, Dict[str, Any]]]:
         try:
-            url = f'{self.odoo_url}/odoo-api/raspberry/authenticate/in_parking'
-            response = await self.make_request(url=url, method='POST', data={'license_plate': license_plate})
+            url = f'{self.odoo_url}/odoo-api/raspberry/authenticate/in_parking?license_plate={license_plate}'
+            response = await self.make_request(url=url, method='POST')
+            return self.check_response(response)
+        except Exception as e:
+            _logger.exception(e)
+
+    async def post_history_got_inside_parking(self, user_id, user_name):
+        try:
+            url = f'{self.odoo_url}/odoo-api/raspberry/authenticate/history?user_id={user_id}?user_name={user_name}?type=in'
+            response = await self.make_request(url=url, method='POST')
             return self.check_response(response)
         except Exception as e:
             _logger.exception(e)
